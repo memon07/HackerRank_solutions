d0131d2bd7e37e8815399e8c24f5db4399663b79
memon07 <memonshoyeb26@gmail.com>
2019-06-07 09:07:06 +0530

    sol btw two sets

---
+++ Problem Solving/Between Two Sets/solution.js
@@ -0,0 +1,13 @@
+function getTotalX(a, b) {
+    let validCount = 0;
+
+    for (let x = 1; x <= 100; x++) {
+        if (a.every(int => (x % int == 0))) {
+            if (b.every(int => (int % x == 0))) {
+                validCount++;
+            }
+        }
+    }
+
+    return validCount;
+}
